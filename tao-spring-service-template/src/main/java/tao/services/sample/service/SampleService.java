db43e70f4b7226dfaa2d2a8ddb66d17dff3f68d6
kraftek <kraftek@c-s.ro>
2019-10-29 19:09:20 +0200

    Changes from Tue 10/29/2019 19:09:20.74

--- tao-spring-service-template/src/main/java/tao/services/sample/service/SampleService.java
+++ tao-spring-service-template/src/main/java/tao/services/sample/service/SampleService.java
@@ -3,13 +3,13 @@ package tao.services.sample.service;
 import org.springframework.stereotype.Service;
 import ro.cs.tao.messaging.Message;
 import ro.cs.tao.messaging.NotifiableComponent;
-import ro.cs.tao.messaging.Topics;
+import ro.cs.tao.messaging.Topic;
 
 @Service("sampleService")
 public class SampleService extends NotifiableComponent {
     @Override
     protected String[] topics() {
-        return new String[] {Topics.INFORMATION };
+        return new String[] {Topic.INFORMATION.value() };
     }
 
     public String sampleMethod() { return "Hello"; }
